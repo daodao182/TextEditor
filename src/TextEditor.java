a52bb4ad1020b3ad85438b293b1912c186d2e9af
yingfan <yingfan027@gmail.com>
2023-02-16 02:07:23 -0700

    scrollPane

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,26 +1,47 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TextEditor extends JFrame {
+public class TextEditor extends JFrame implements ActionListener {
 
 
-    public static void main(String[] args) {
-        JFrame f=new JFrame("Editor");
+    public TextEditor(){
+        this.setTitle("baobao's editor");
+        this.setBackground(Color.blue);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,400);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+
         JMenuBar mb=new JMenuBar();
         JMenu m1=new JMenu("Edit");
-        JMenu m2=new JMenu("Close");
         mb.add(m1);
-        mb.add(m2);
+        this.setJMenuBar(mb);
 
-        JPanel p=new JPanel();
-        p.setBackground(Color.pink);
-        p.add(mb);
-        f.add(p);
-        f.setSize(400,400);
-        f.setJMenuBar(mb);
-        f.setVisible(true);
+        JTextArea textArea=new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("copperplate",Font.PLAIN,220));
+
+        JScrollPane scrollPane=new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(750,350));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        this.add(scrollPane);
+
+        this.setVisible(true);
+
+
+
+    }
+    public static void main(String[] args) {
+        new TextEditor();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
 
 
