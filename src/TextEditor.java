8f75d620a4f7ccbea2a75fc60b662c0aca977178
yingfan <yingfan027@gmail.com>
2023-02-15 22:56:53 -0700

    frame

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,5 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditor extends JFrame {
-    
+
+
+    public static void main(String[] args) {
+        JFrame f=new JFrame("Editor");
+        JMenuBar mb=new JMenuBar();
+        JMenu m1=new JMenu("Edit");
+        JMenu m2=new JMenu("Close");
+        mb.add(m1);
+        mb.add(m2);
+
+        JPanel p=new JPanel();
+        p.setBackground(Color.pink);
+        p.add(mb);
+        f.add(p);
+        f.setSize(400,400);
+        f.setJMenuBar(mb);
+        f.setVisible(true);
+    }
+
 }
+
+
