d19f3671d4f7111cb2f9d58122a68fa2cce84e5f
yingfan <yingfan027@gmail.com>
2023-02-18 01:58:44 -0700

    Friday

--- src/texteitor/Main.java
+++ src/texteitor/Main.java
@@ -1,38 +1,15 @@
 package texteitor;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Main extends JFrame implements ActionListener {
-    TextEditor tE;
-    Topbar bar;
-    public Main(){
-
-
-        TextEditor tE = new TextEditor();
-        Topbar bar = new Topbar();
-        tE.add(bar);
-        this.add(tE);
-
-        this.setVisible(true);
-    }
-
+public class Main {
 
 
     public static void main(String[] args) {
-
-       new Main();
-
+        new TextEditor();
+//        new Topbar();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String s=e.getActionCommand();
-        if(s.equals("cut")){
-            tE.textArea.cut();
 
-        }
-    }
 }
+
+
--- src/texteitor/TextEditor.java
+++ src/texteitor/TextEditor.java
@@ -1,11 +1,24 @@
 package texteitor;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
-public class TextEditor extends JFrame  {
+public class TextEditor extends JFrame implements ActionListener {
     JTextArea textArea;
-
+    JMenuBar mb;
+    JMenu m1,m2;
+    JMenuItem item1,item2,item3,item4,item5,item6,item7,item8;
 
     public TextEditor(){
         this.setTitle("baobao's editor");
@@ -14,26 +27,119 @@ public class TextEditor extends JFrame  {
         this.setSize(800,400);
         this.setLayout(new FlowLayout());
 
-        JTextArea textArea=new JTextArea();
+        textArea=new JTextArea();
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
-        textArea.setFont(new Font("copperplate",Font.PLAIN,220));
+        textArea.setFont(new Font("copperplate",Font.PLAIN,20));
 
         JScrollPane scrollPane=new JScrollPane(textArea);
         scrollPane.setPreferredSize(new Dimension(750,350));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-
         this.add(scrollPane);
+
+        mb=new JMenuBar();
+        m1=new JMenu("File");
+        item4=new JMenuItem("open");
+        item5=new JMenuItem("save");
+        item6=new JMenuItem("print");
+        item8=new JMenuItem("exit");
+        item4.addActionListener(this );
+        item5.addActionListener(this );
+        item6.addActionListener(this );
+        item8.addActionListener(this );
+        m1.add(item4);
+        m1.add(item5);
+        m1.add(item6);
+        m1.add(item8);
+        mb.add(m1);
+        m2=new JMenu("Edit");
+        item1=new JMenuItem("cut");
+        item2=new JMenuItem("copy");
+        item3=new JMenuItem("paste");
+        item7=new JMenuItem("highlight");
+        item1.addActionListener(this );
+        item2.addActionListener(this );
+        item3.addActionListener(this );
+        item7.addActionListener(this);
+        m2.add(item1);
+        m2.add(item2);
+        m2.add(item3);
+        m2.add(item7);
+        mb.add(m2);
+
+        this.setJMenuBar(mb);
+
         this.setVisible(true);
-
-
 
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource()==item1){
+            textArea.cut();
+        }else if(e.getSource()==item2){
+            textArea.copy();
+        }else if(e.getSource()==item3){
+            textArea.paste();
+        }else if(e.getSource()==item4){
+            JFileChooser fileChooser=new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter=new FileNameExtensionFilter("text file","txt");
+            fileChooser.setFileFilter(filter);
+            int response=fileChooser.showOpenDialog(null);
+            if (response==JFileChooser.APPROVE_OPTION){
+                File file;
+                file=new File(fileChooser.getSelectedFile().getPath());
+            }
+        }
+        else if(e.getSource()==item5){
+            JFileChooser fileChooser=new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            int response=fileChooser.showSaveDialog(null);
+            if(response==JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter writer=null;
+                file=new File(fileChooser.getSelectedFile().getPath());
+                try {
+                    writer=new PrintWriter(file);
+                    writer.println(textArea.getText());
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                finally {
+                    writer.close();
+                }
+
+            }
+        }else if(e.getSource()==item6){
+            try {
+                textArea.print();
+            } catch (PrinterException ex) {
+                ex.printStackTrace();
+            }
+        }else if(e.getSource()==item7){
+            Highlighter highlighter=textArea.getHighlighter();
+            Highlighter.HighlightPainter painter=new DefaultHighlighter.DefaultHighlightPainter(Color.red);
+            highlighter.removeAllHighlights();
+            String text=textArea.getText().toUpperCase();
+            for (int i = 0; i < text.length(); i++) {
+
+                    try {
+
+                        highlighter.addHighlight(i, i + 1, painter);
+                    } catch (BadLocationException ble) {
+                    }
 
 
+            }
+        }else if(e.getSource()==item8){
+            System.exit(0);
+        }
+
+
+    }
 }
 
 
