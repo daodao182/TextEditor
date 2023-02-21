3300dbb6092c68ac823acc3f660d364b3a917cb5
yingfan <yingfan027@gmail.com>
2023-02-20 19:08:11 -0700

    update Textarea and Topbar

--- src/texteitor/Textarea.java
+++ src/texteitor/Textarea.java
@@ -3,18 +3,21 @@ package texteitor;
 import javax.swing.*;
 import java.awt.*;
 
-public class Textarea extends TextEditor{
-    JTextArea textArea;
+public class Textarea extends JTextArea{
+
 
 
 
     Textarea(){
 
-        textArea=new JTextArea();
-        textArea.setText("asdafwef");
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        textArea.setFont(new Font("copperplate",Font.PLAIN,20));
+
+        this.setText("asdafwef");
+        this.setLineWrap(true);
+        this.setWrapStyleWord(true);
+        this.setFont(new Font("copperplate",Font.PLAIN,20));
+        JScrollPane scrollPane=new JScrollPane(this);
+        scrollPane.setPreferredSize(new Dimension(750,350));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
 
     }
--- src/texteitor/Topbar.java
+++ src/texteitor/Topbar.java
@@ -2,40 +2,39 @@ package texteitor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 
 public class Topbar extends JMenuBar{
-    JMenuBar mb;
+
     private JMenu m1,m2;
     private JMenuItem item1,item2,item3,item4,item5,item6,item7,item8,item9;
 
 
     Topbar(){
-        super();
 
-        mb=new JMenuBar();
         m1=new JMenu("File");
         item4=new JMenuItem("open");
-//        item5=new JMenuItem("save");
-//        item6=new JMenuItem("print");
-//        item8=new JMenuItem("exit");
-//        item4.addActionListener(this );
-//        item5.addActionListener(this );
-//        item6.addActionListener(this );
-//        item8.addActionListener(this );
+        item5=new JMenuItem("save");
+        item6=new JMenuItem("print");
+        item8=new JMenuItem("exit");
+//        item4.addActionListener((ActionListener) this);
+//        item5.addActionListener((ActionListener) this);
+//        item6.addActionListener((ActionListener) this);
+//        item8.addActionListener((ActionListener) this);
         m1.add(item4);
-//        m1.add(item5);
-//        m1.add(item6);
-//        m1.add(item8);
-        mb.add(m1);
+        m1.add(item5);
+        m1.add(item6);
+        m1.add(item8);
+        this.add(m1);
         m2=new JMenu("Edit");
         item1=new JMenuItem("cut");
-//        item2=new JMenuItem("copy");
-//        item3=new JMenuItem("paste");
-//        item7=new JMenuItem("highlight");
-//        item9=new JMenuItem("search");
-//        item1.addActionListener(this );
-//        item2.addActionListener(this );
+        item2=new JMenuItem("copy");
+        item3=new JMenuItem("paste");
+        item7=new JMenuItem("highlight");
+        item9=new JMenuItem("search");
+//        item1.addActionListener((ActionListener) this);
+//        item2.addActionListener((ActionListener) this);
 //        item3.addActionListener(this );
 //        item7.addActionListener(this);
 //        item9.addActionListener(this);
@@ -44,7 +43,7 @@ public class Topbar extends JMenuBar{
         m2.add(item3);
         m2.add(item7);
         m2.add(item9);
-        mb.add(m2);
+        this.add(m2);
 
 
     }
